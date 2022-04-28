1000ba634dd0b67155b669d7abfc6ff9f30bff26
a2tt <usera2tt@gmail.com>
2022-04-29 03:10:06 +0900

    feat: add cors middleware

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,9 @@
 import importlib
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 
+from configs import global_settings
 from server import routers
 from server.utils import os_util
 from server.utils.response import api_response, api_error_response
@@ -10,6 +12,21 @@ from server.websocket import create_websocket
 
 app = FastAPI()
 sio, sio_app = create_websocket(app)
+
+origins = ['https://together-coding.com']
+if global_settings.DEBUG:
+    origins.extend([
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+    ])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['X-API-KEY']
+)
 
 for router_mod in routers.__all__:
     router = importlib.import_module(f'.routers.{router_mod}', package=__name__)
